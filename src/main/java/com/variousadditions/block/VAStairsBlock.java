0169ef6245a713833688580c87f46b3f165bc8ad
Rellit <53947335+Rellit-219@users.noreply.github.com>
2019-08-25 16:39:32 -0700

    Added Block file

---
+++ src/main/java/com/variousadditions/block/VAStairsBlock.java
@@ -0,0 +1,13 @@
+package com.variousadditions.block;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.StairsBlock;
+
+public class VAStairsBlock extends StairsBlock {
+
+	public VAStairsBlock(BlockState state, Properties properties) {
+		super(state, properties);
+		
+	}
+	
+}
